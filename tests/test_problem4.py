32551e99911204a2ef2acf6c0572f85188de02b5
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-02-16 10:25:14 -0800

    Solution

--- tests/test_problem4.py
+++ tests/test_problem4.py
@@ -4,4 +4,4 @@ from euler.problem4 import Problem4
 
 class TestEngine(unittest.TestCase):
     def test_shouldReturnSolution(self):
-        self.assertEqual(906609, Problem4().palindrome())
+        self.assertEqual(906609, Problem4(100, 1000).palindrome())
